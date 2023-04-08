101898807b33cbd4bf6aa1035db3a0b61a5525dc
yantingChi <chi00068@umn.edu>
2023-04-07 21:22:48 -0500

    mlta

---
+++ mlta/build/lib/CMakeFiles/AnalyzerObj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AnalyzerObj.dir/Analyzer.cc.o"
+  "CMakeFiles/AnalyzerObj.dir/CallGraph.cc.o"
+  "CMakeFiles/AnalyzerObj.dir/Common.cc.o"
+  "CMakeFiles/AnalyzerObj.dir/Config.cc.o"
+  "CMakeFiles/AnalyzerObj.dir/MLTA.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalyzerObj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
